6694842295b463f7fc3ece1af1cb171510528c10
Andrey <andry@logvin.net>
2017-10-27 19:28:21 +0300

    Implemented pause script

---
+++ Assets/Scripts/OnOffPauseScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnOffPauseScript : MonoBehaviour {
+
+    private bool isPaused;
+
+    public float TimeSpeed;
+
+    public GameObject PauseButton;
+    public GameObject ResumeButton;
+
+    public GameObject PauseCanvas;
+    public GameObject GameCanvas;
+
+    private void Pause()
+    {
+        GameCanvas.SetActive(false);
+        PauseCanvas.SetActive(true);
+        TimeSpeed = 0;
+    }
+
+    private void Resume()
+    {
+        PauseCanvas.SetActive(false);
+        GameCanvas.SetActive(true);
+        TimeSpeed = 1;
+    }
+
+    void Start()
+    {
+        isPaused = false;
+        TimeSpeed = 1;
+
+        ResumeButton.GetComponent<Button>().onClick.AddListener(Resume);
+        PauseButton.GetComponent<Button>().onClick.AddListener(Pause);
+    }
+
+    void Update()
+    {
+        Time.timeScale = TimeSpeed;
+    }
+}
