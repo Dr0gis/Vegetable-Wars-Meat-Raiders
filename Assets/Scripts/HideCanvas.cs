647ceb6b34a3803600714cfc909d399bcaec7efd
Костянтин Грінько <kostiantyn.hrinko@kture.edu.ua>
2017-10-31 21:14:08 +0300

    Merge branch 'GameplayMenuScene'

---
+++ Assets/Scripts/HideCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideCanvas : MonoBehaviour {
+
+    public GameObject menu;
+
+    // Use this for initialization
+    void Start () {
+        menu.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
