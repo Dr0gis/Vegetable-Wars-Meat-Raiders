937791dacedb30d28182efd348aa256892ced4fe
Igor Dr0gis <dr0gis@mail.ru>
2017-11-01 20:57:11 +0200

    Add function Set Score Text

---
+++ Assets/Scripts/ScoreChanges.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreChanges : MonoBehaviour
+{
+    public Text Score;
+
+    public void SetTextScore(string ScoreValue)
+    {
+        Score.text = ScoreValue;
+    }
+}
