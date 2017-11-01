aaf8179ae70b8fbf93d20d42114e0352dd2f0f63
MAF1N <nikita_yarovoy@ukr.net>
2017-11-01 23:02:17 +0200

    Pause Menu Functions Added

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic; 
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour {
-	
 
-	public void ChangeScene (string scene) {
+    public void ChangeScene(string scene)
+    {
         SceneManager.LoadScene(scene, LoadSceneMode.Single);
     }
 
