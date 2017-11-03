7b98e10d34297be73be36ff9cde38e7bd153176c
Valerii Nikolaienko <valeranikolaenko17@gmail.com>
2017-11-03 18:38:12 +0200

    Added Vegetable Classes (not finished)

---
+++ Assets/Scripts/Vegetables/VegetableClass.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+public abstract class VegetableClass { 
+
+    public int Health;
+    public int Damage;
+    public float Speed;
+    public string Prefab;
+    public GameObject CurrentGameObject;
+
+    public VegetableClass()
+    {
+        Health = 1;
+        Damage = 0;
+        Speed = 1;
+        Prefab = null;
+        CurrentGameObject = null;
+    }
+
+    public VegetableClass(int health, int damage, float speed, string prefab, GameObject gameObject)
+    {
+        Health = health;
+        Damage = damage;
+        Speed = speed;
+        Prefab = prefab;
+        CurrentGameObject = gameObject;
+    }
+
+    public void OnCollision2D(Collision2D collider)
+    {
+        switch (collider.gameObject.tag)
+        {
+            case "Block":
+                //collider.gameObject.GetComponent<BlockClass>().Health -= Damage;
+                break;
+                //.....
+        }
+    }
+
+    public void OnDestroy()
+    {
+        CurrentGameObject.GetComponent<VegetableController>().CallDestroy();
+    }
+
+    public abstract void UseSpecialAbility();
+
+    public void Check()
+    {
+        if (Health <= 0)
+        {
+            OnDestroy();
+        }
+        if (CurrentGameObject.GetComponent<Rigidbody2D>().velocity == Vector2.zero)
+        {
+            OnDestroy();
+        }
+    }
+}
---
+++ Assets/Scripts/Vegetables/VegetableController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VegetableController : MonoBehaviour
+{
+
+    private bool isShoted;
+    private Rigidbody2D rigidbody2D;
+
+
+	void Start ()
+	{
+	    isShoted = false;
+	    rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+    public void Shoot()
+    {
+        isShoted = true;
+        //add velocity here
+    }
+
+    public void CallDestroy()
+    {
+        Destroy(gameObject);
+    }
+	
+	void Update ()
+    {
+	    gameObject.GetComponent<VegetableClass>().Check();
+        if (Input.touchCount>0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            gameObject.GetComponent<VegetableClass>().UseSpecialAbility();
+        }
+	}
+}
