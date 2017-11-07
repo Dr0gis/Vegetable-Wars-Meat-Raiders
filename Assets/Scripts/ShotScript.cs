6939be8ca767af419156cc942d9e56350308692e
Andrey <andry@logvin.net>
2017-11-07 23:38:34 +0200

    Almost implemented visualization

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -14,13 +14,15 @@ public class ShotScript : MonoBehaviour
 
     public ObjectManagerScript vegetableManager;
 
-    public float MaxTensionByX = 200;
-    public float MaxTensionByY = 200;
-    public float MinTensionByX = 50;
-    public float MinTensionByY = 50;
-    public float StrengthScale;
+    public float MaxTensionByX = 300;
+    public float MaxTensionByY = 300;
+    public float MinTensionByX = 25;
+    public float MinTensionByY = 25;
+    public float StrengthScale = 0.05f;
 
     public GameObject CurrentVegetable;
+
+    public int PredictionStepsNumber = 250;
 
     private Touch? GetShotTouch()
     {
@@ -49,7 +51,26 @@ public class ShotScript : MonoBehaviour
 
     private void Visualize(Vector2 pushVector)
     {
-        // Visualize heare
+        LineRenderer sightLine = GetComponent<LineRenderer>();
+        Rigidbody2D rigidbody = CurrentVegetable.GetComponent<Rigidbody2D>();
+        Vector2 pos = new Vector2(CurrentVegetable.transform.position.x, CurrentVegetable.transform.position.y);
+
+        float timestep = Time.fixedDeltaTime / Physics2D.velocityIterations;
+        Vector2 gravityAccel = Physics2D.gravity * rigidbody.gravityScale * timestep * timestep;
+        float drag = 1f - timestep * rigidbody.drag;
+        Vector2 moveStep = pushVector * timestep;
+
+        sightLine.positionCount = PredictionStepsNumber;
+
+        sightLine.SetPosition(0, pos);
+        for (int i = 1; i < PredictionStepsNumber; ++i)
+        {
+            moveStep += gravityAccel;
+            moveStep *= drag;
+            pos += moveStep;
+
+            sightLine.SetPosition(i, pos);
+        }
     }
 
     private GameObject GetNextDefaultVegetable()
@@ -68,6 +89,7 @@ public class ShotScript : MonoBehaviour
 
     private void MakeShot(Vector2 pushVector)
     {
+        GetComponent<LineRenderer>().positionCount = 0;
         CurrentVegetable.GetComponent<VegetableController>().Shoot(pushVector);
     }
 
@@ -124,7 +146,10 @@ public class ShotScript : MonoBehaviour
 
         TakeAim(out shotVector, out isShotMade);
 
-        if (shotVector.HasValue && (Mathf.Abs(shotVector.Value.x) > MinTensionByX || Mathf.Abs(shotVector.Value.y) > MinTensionByY))
+        if (CurrentVegetable != null &&
+            CurrentVegetable.GetComponent<VegetableController>().IsShoted == false &&
+            shotVector.HasValue && 
+            (Mathf.Abs(shotVector.Value.x) > MinTensionByX || Mathf.Abs(shotVector.Value.y) > MinTensionByY))
         {
             Vector2 pushVector = PrepareVector(shotVector.Value);
             if (isShotMade == false)
