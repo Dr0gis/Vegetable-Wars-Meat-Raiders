50ae21faf31f6b4dec5f4213b672d864b8ca8558
Igor Dr0gis <dr0gis@mail.ru>
2017-11-10 22:17:20 +0200

    Code refactor

--- Assets/Scripts/SettingsPauseMenu.cs
+++ Assets/Scripts/SettingsPauseMenu.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class SettingsPauseMenu : MonoBehaviour {
+public class SettingsPauseMenu : MonoBehaviour
+{
     public Button SkipButton;
-	void Start () {
+    public Button MenuButton;
+    public Button RetryButton;
+    public string Scene;
+
+	void Start ()
+    {
         SkipButton.interactable = false;
-	}
-	
+        MenuButton.onClick.AddListener(menuButtonListener);
+        RetryButton.onClick.AddListener(retryButtonListener);
+    }
+
+    private void menuButtonListener()
+    {
+        SceneManager.LoadScene(Scene);
+    }
+    private void retryButtonListener()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
 }
---
+++ Assets/Scripts/SettingsUILevelSelection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SettingsUILevelSelection : MonoBehaviour
+{
+    public Button BackButton;
+
+    public string BackScene;
+
+	void Start ()
+    {
+        BackButton.onClick.AddListener(backButtonListener);
+    }
+
+    private void backButtonListener()
+    {
+        SceneManager.LoadScene(BackScene);
+    }
+}
