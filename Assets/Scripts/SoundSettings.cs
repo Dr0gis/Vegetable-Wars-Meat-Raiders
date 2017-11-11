5f33ebb7574f4106a80511851e0113aa534c0635
MAF1N <nikita_yarovoy@ukr.net>
2017-11-11 20:05:20 +0200

    Fixed Sound

---
+++ Assets/Scripts/SoundSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSettings : MonoBehaviour
+{
+    public GameObject clickSoundPref;
+    public List<Button> Buttons;
+    void Start()
+    {
+        clickSoundPref.GetComponent<AudioSource>().enabled = true;
+        foreach (var button in Buttons)
+        {
+            button.onClick.AddListener(ClickSoundListener);
+        }
+    }
+    public void ClickSoundListener()
+    {       
+        clickSoundPref.GetComponent<AudioSource>().PlayOneShot(clickSoundPref.GetComponent<AudioSource>().clip);
+    }
+}
