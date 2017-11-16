eaf53e224564200846859845a0dd3127a779596d
MAF1N <nikita_yarovoy@ukr.net>
2017-11-16 20:23:10 +0200

    Sound and Music Proved

--- Assets/Scripts/SoundSettings.cs
+++ Assets/Scripts/SoundSettings.cs
@@ -4,21 +4,33 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SoundSettings : MonoBehaviour
 {
-    public GameObject clickSoundPref;
+
     public List<Button> Buttons;
     void Start()
     {
-        clickSoundPref.GetComponent<AudioSource>().enabled = true;
         foreach (var button in Buttons)
         {
             button.onClick.AddListener(ClickSoundListener);
         }
+        SoundManager.PlayMusic("Nordic Title");
+        
     }
     public void ClickSoundListener()
-    {       
-        clickSoundPref.GetComponent<AudioSource>().PlayOneShot(clickSoundPref.GetComponent<AudioSource>().clip);
+    {
+        SoundManager.PlaySoundUI("clickSound");
+    }
+    private void OnLevelWasLoaded(int level)
+    {
+        SceneLoad();
+    }
+    public void SceneLoad()
+    {
+        var Scene = SceneManager.GetActiveScene();
+        if(Scene.isLoaded == true)
+            SoundManager.PlaySound("LevelStartSound");
     }
 }
