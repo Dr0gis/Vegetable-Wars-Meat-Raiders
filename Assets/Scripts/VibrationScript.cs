51f015fd75531be12689420ab42c4f6d729239ef
MAF1N <nikita_yarovoy@ukr.net>
2017-11-17 18:40:57 +0200

    Vibration settings added
    using static field "IsOn" you now can switch vibration on\off

--- Assets/Scripts/VibrationScript.cs
+++ Assets/Scripts/VibrationScript.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class VibrationScript : MonoBehaviour {
 
+    private static bool isOn;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        isOn = true;	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+    public static bool IsOn
+    {
+        get
+        {
+            return isOn;
+        }
+        set
+        {
+            isOn = value;
+        }
+    }
 
     void OnDestroy()
     {
-        Handheld.Vibrate();
+        if (isOn)
+        { 
+            Handheld.Vibrate();
+        }
     }
 
 }
