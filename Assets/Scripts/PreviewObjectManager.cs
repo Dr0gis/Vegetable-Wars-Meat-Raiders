bc130af15ee7eca67aec55ac44021e307bd065f9
Valerii Nikolaienko <valeranikolaenko17@gmail.com>
2017-11-22 19:32:45 +0200

    Linked Preview Scene to Vegetables Select Scene

--- Assets/Scripts/PreviewObjectManager.cs
+++ Assets/Scripts/PreviewObjectManager.cs
@@ -3,33 +3,30 @@ using System.Collections.Generic;
 using Assets.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PreviewObjectManager : MonoBehaviour {
 
     public List<BlockClass> AvailableBlocks;
     public List<MeatClass> AvailableMeats;
     public int CurrentLevel;
+    public Button ShopButton;
 
     public List<BlockClass> GetAvailableBlocks()
     {
         List<BlockClass> listBlocks = GetComponent<PullObjects>().Levels[CurrentLevel].Blocks;
-
         return listBlocks;
     }
 
     public List<MeatClass> GetAvailableMeats()
     {
         List<MeatClass> listMeats = GetComponent<PullObjects>().Levels[CurrentLevel].Meats;
-
         return listMeats;
     }
 
     public void Initiate()
     {
         GetComponent<PullObjects>().Initiate();
-
-        GameObject catapult = GameObject.Find("Catapult");
-
         AvailableBlocks = GetAvailableBlocks();
         foreach (var block in AvailableBlocks)
         {
@@ -48,5 +45,12 @@ public class PreviewObjectManager : MonoBehaviour {
     void Start()
     {
         Initiate();
+        ShopButton.onClick.AddListener(ShopButtonListener);
+    }
+
+    private void ShopButtonListener()
+    {
+        //save camera size and position here
+        SceneManager.LoadScene("VegetablesSelection");
     }
 }
---
+++ Assets/Scripts/VegetablesSelectScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VegetablesSelectScript : MonoBehaviour {
+
+    public Button PreviewButton;
+    public Button PlayButton;
+    public Button BackButton;
+
+    private Scene currentScene;
+    private Scene previewScene;
+
+	void Start ()
+    {
+        PlayButton.onClick.AddListener(PlayButtonListener);
+        BackButton.onClick.AddListener(() => SceneManager.LoadScene("LevelSelection"));
+        PreviewButton.onClick.AddListener(PreviewButtonListener);
+	}
+	
+    private void PlayButtonListener()
+    {
+        //return selected vegetables and other parameters to level
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void PreviewButtonListener()
+    {
+        //save selected vegetables here
+        SceneManager.LoadScene("PreviewScene");
+    }
+
+    void Update ()
+    {
+	    	
+	}
+}
